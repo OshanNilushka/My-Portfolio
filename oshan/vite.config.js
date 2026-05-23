f8c2e0ebd9dd0b1ed31028d233089a0f6b2452af
Oshan Nilushka <oshannilushka27@gmail.com>
2026-05-23 20:00:57 +0530

    updated

--- oshan/vite.config.js
+++ oshan/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite" 
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, 'src/components'),
     },
   },
 })
